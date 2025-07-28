77246b015697a1c71a59d3e3e7d9835000bd2fd1
Euler Lobo <eulerlobo@gmail.com>
2025-07-28 10:01:08 -0300

    feat: java restful starter

---
+++ java_restful/src/main/java/com/financialmonitor/FinancialMonitor/FinancialMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.financialmonitor.FinancialMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialMonitorApplication.class, args);
+	}
+
+}
---
+++ java_restful/src/test/java/com/financialmonitor/FinancialMonitor/FinancialMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialmonitor.FinancialMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
